8c6f0d59ed9541e0571fbb82aa4168598aa5be02
alex <aleksei.lancevv@gmail.com>
2025-09-30 20:52:06 +0500

    create globalHeader

---
+++ src/components/header/globalHeader/index.tsx
@@ -0,0 +1,9 @@
+<GlobalHeader>
+  <TopBar>
+    <FreshnessBadge text="Гарантия свежести" />
+    <UtilityLinks links={[...]} />
+    <CitySelector currentCity="Санкт-Петербург" />
+    <PhoneLink number="+7 495 668-07-30" />
+    <CartWidget itemCount={3} />
+  </TopBar>
+</GlobalHeader>
---
+++ src/components/header/globalHeader/topBar/smallOurAdvantages/index.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+
+interface SmallOurAdvantagesTypeProps {
+	title: string;
+	icon: ReactNode;
+}
+
+const SmallOurAdvantages = ({ title, icon }: SmallOurAdvantagesTypeProps) => {
+
+}
+
+export default SmallOurAdvantages;
