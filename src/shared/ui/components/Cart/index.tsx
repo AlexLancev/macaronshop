aa240d7db11d2baa90f861cfbc8b891b81321c2f
alex <aleksei.lancevv@gmail.com>
2025-10-15 19:40:43 +0500

    fix cart

--- src/shared/ui/components/Cart/index.tsx
+++ src/shared/ui/components/Cart/index.tsx
@@ -1,16 +1,15 @@
-import { ShoppingCart } from 'lucide-react'
+import { ShoppingBag as ShoppingBagIcon } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
-import { calcQuantityGoods } from './utils/calcQuantityGoods'
 
 export default function Cart() {
 	return (
-		<Link className='flex items-center gap-x-2' href='/cart'>
+		<Link className='flex items-center gap-x-4' href='/cart'>
 			<div className="relative">
-				<ShoppingCart />
-				<span className='absolute -right-2 -bottom-2 rounded-full bg-accent-foreground text-white p-1 text-xs'>{1}</span>
+				<ShoppingBagIcon size={24} stroke="#333333" strokeWidth={1} />
+				<span className='absolute -right-2 -bottom-2 px-1.25 h-4 rounded-full bg-accent-foreground text-white text-xs grid place-items-center'>{0}</span>
 			</div>
-			В корзине {calcQuantityGoods(1)}
+			В корзину
 		</Link>
 	)
 }
