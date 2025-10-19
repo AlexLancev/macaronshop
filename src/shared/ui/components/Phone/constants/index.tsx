de3ec6f312f4a35660c252a97857a76e72cc6a58
alex <aleksei.lancevv@gmail.com>
2025-10-19 20:44:22 +0500

    create component phone

--- src/shared/ui/components/Phone/constants/index.tsx
+++ src/shared/ui/components/Phone/constants/index.tsx
@@ -1,8 +1,8 @@
-import { Smartphone } from "lucide-react";
+import { Smartphone as SmartphoneIcon } from "lucide-react";
 
 export const phoneData = {
 	mobilePhone: {
-		icon: <Smartphone />,
+		icon: <SmartphoneIcon size={24} stroke="#333333" strokeWidth={1} />,
 		patchPhone: 74956680730,
 		namePhone: "+7 495 668-07-30",
 		title: "Позвонить нам",
