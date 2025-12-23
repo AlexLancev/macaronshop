0fd8eb5823250970f5359b67086948d129ad8f3d
alex <aleksei.lancevv@gmail.com>
2025-12-23 20:03:25 +0500

    finished eclairsData

--- src/shared/lib/constants/catalogDesserts/macaron/index.tsx
+++ src/shared/lib/constants/catalogDesserts/macaron/index.tsx
@@ -1016,39 +1016,39 @@ export const eclairsData = {
 				flavorName: 'Лесной орех',
 				quantity: 6,
 			},
-						{
+			{
 				flavorName: 'Фисташка',
 				quantity: 6,
 			},
-						{
+			{
 				flavorName: 'Капучино',
 				quantity: 6,
 			},
-						{
+			{
 				flavorName: 'Шоколад-апельсин',
 				quantity: 6,
 			},
-						{
+			{
 				flavorName: 'Лимон',
 				quantity: 6,
 			},
-						{
+			{
 				flavorName: 'Ананас',
 				quantity: 6,
 			},
-						{
+			{
 				flavorName: 'Малина',
 				quantity: 6,
 			},
-						{
+			{
 				flavorName: 'Лаванда',
 				quantity: 6,
 			},
-						{
+			{
 				flavorName: 'Эклеры',
 				quantity: 8,
 			},
-						{
+			{
 				flavorName: 'Картошка',
 				quantity: 7,
 			},
@@ -1067,19 +1067,19 @@ export const eclairsData = {
 				flavorName: 'Вишня',
 				quantity: 1,
 			},
-						{
+			{
 				flavorName: 'Карамель',
 				quantity: 1,
 			},
-						{
+			{
 				flavorName: 'Смородина',
 				quantity: 1,
 			},
-						{
+			{
 				flavorName: 'Манго',
 				quantity: 1,
 			},
-						{
+			{
 				flavorName: 'Ваниль',
 				quantity: 1,
 			},
@@ -1087,145 +1087,218 @@ export const eclairsData = {
 		typeHoliday: 'newYear',
 		prodDetails: prodDetailsData['eclairs'],
 	},
-	a: {
-		id: ,
-		title: '',
-		description: '',
-		price: ,
+	paradise: {
+		id: 3,
+		title: 'Набор с эклерами «Райский»',
+		description: '12 макаронc (лесной орех, лимон, вишня, малина) + 3 эклера (смородина, манго, ваниль) в коробке.',
+		price: 1390,
 		gallery: ['/images/eclairs/',],
 		flavor: [
 			{
-				flavorName: '',
-				quantity: ,
-			},],
-		typeHoliday: 'nationalUnityDay',
+				flavorName: 'Макарони Лесной орех',
+				quantity: 3,
+			},
+			{
+				flavorName: 'Макарони Пряная вишня',
+				quantity: 3,
+			},
+			{
+				flavorName: 'Макарони Малина',
+				quantity: 3,
+			},
+			{
+				flavorName: 'Макарони Лимон',
+				quantity: 3,
+			},
+			{
+				flavorName: 'Эклер Смородина',
+				quantity: 1,
+			},
+			{
+				flavorName: 'Эклер Манго',
+				quantity: 1,
+			},
+			{
+				flavorName: 'Эклер Ваниль',
+				quantity: 1,
+			},
+		],
+		typeHoliday: 'valentinesDay',
 		prodDetails: prodDetailsData['eclairs'],
 	},
-	a: {
-		id: ,
-		title: '',
-		description: '',
-		price: ,
+	sonata: {
+		id: 4,
+		title: 'Набор эклеров Соната',
+		description: '5 вкусов в наборе: Ванильный, Шоколадный, Вишневый, Карамельный и Фундучное пралине.',
+		price: 910,
 		gallery: ['/images/eclairs/',],
 		flavor: [
 			{
-				flavorName: '',
-				quantity: ,
-			},],
-		typeHoliday: 'nationalUnityDay',
+				flavorName: 'Фундучное пралине',
+				quantity: 1,
+			},
+			{
+				flavorName: 'Ваниль',
+				quantity: 1,
+			},
+			{
+				flavorName: 'Шоколад',
+				quantity: 1,
+			},
+			{
+				flavorName: 'Вишня',
+				quantity: 1,
+			},
+			{
+				flavorName: 'Карамель',
+				quantity: 1,
+			},
+		],
+		typeHoliday: 'fatherlandDay',
 		prodDetails: prodDetailsData['eclairs'],
 	},
-	a: {
-		id: ,
-		title: '',
-		description: '',
-		price: ,
+	izuminka: {
+		id: 5,
+		title: 'Набор с эклерами «Изюминка»',
+		description: '10 макаронc (кокосовый рай, фисташка, пломбир, соленая карамель, лесной орех) + 3 эклера (сицилийская фисташка, фундучное пралине) в коробке.',
+		price: 1390,
 		gallery: ['/images/eclairs/',],
 		flavor: [
 			{
-				flavorName: '',
-				quantity: ,
-			},],
-		typeHoliday: 'nationalUnityDay',
+				flavorName: 'Эклер Сицилийская Фисташка',
+				quantity: 2,
+			},
+			{
+				flavorName: 'Эклер Фундучное Пралине',
+				quantity: 1,
+			},
+			{
+				flavorName: 'Макарони Фисташка',
+				quantity: 2,
+			},
+			{
+				flavorName: 'Макарони Ванильный пломбир',
+				quantity: 2,
+			},
+			{
+				flavorName: 'Макарони Лесной орех',
+				quantity: 2,
+			},
+			{
+				flavorName: 'Макарони Соленая карамель',
+				quantity: 2,
+			},
+			{
+				flavorName: 'Макарони Кокосовый рай',
+				quantity: 2,
+			},
+		],
+		typeHoliday: 'womensDay',
 		prodDetails: prodDetailsData['eclairs'],
 	},
-	a: {
-		id: ,
-		title: '',
-		description: '',
-		price: ,
+	confetti: {
+		id: 6,
+		title: 'Набор с эклерами «Конфетти»',
+		description: '10 макаронc (пломбир, кокосовый рай, смородина, лаванда, бабл-гам) + 3 эклера (манго, карамель, смородина) в коробке.',
+		price: 1390,
 		gallery: ['/images/eclairs/',],
 		flavor: [
 			{
-				flavorName: '',
-				quantity: ,
-			},],
-		typeHoliday: 'nationalUnityDay',
+				flavorName: 'Эклер Смородиновый',
+				quantity: 1,
+			},
+			{
+				flavorName: 'Эклер Манго',
+				quantity: 1,
+			},
+			{
+				flavorName: 'Эклер Карамельный',
+				quantity: 1,
+			},
+			{
+				flavorName: 'Макарони Кокосовый рай',
+				quantity: 2,
+			},
+			{
+				flavorName: 'Макарони Ванильный пломбир',
+				quantity: 2,
+			},
+			{
+				flavorName: 'Макарони Смородина',
+				quantity: 2,
+			},
+			{
+				flavorName: 'Макарони Бабл-гам',
+				quantity: 2,
+			},
+			{
+				flavorName: 'Макарони Лаванда',
+				quantity: 2,
+			},
+		],
+		typeHoliday: 'laborDay',
 		prodDetails: prodDetailsData['eclairs'],
 	},
-	a: {
-		id: ,
-		title: '',
-		description: '',
-		price: ,
+	brightness: {
+		id: 7,
+		title: 'Набор эклеров «Яркий»',
+		description: '5 вкусов в наборе: Ягодный восторг, Кокосовый рай, Апельсиновый брауни, Лавандовая черника и Фундучное пралине.',
+		price: 910,
 		gallery: ['/images/eclairs/',],
 		flavor: [
 			{
-				flavorName: '',
-				quantity: ,
-			},],
-		typeHoliday: 'nationalUnityDay',
+				flavorName: 'Ягодный восторг',
+				quantity: 1,
+			},
+			{
+				flavorName: 'Кокосовый рай',
+				quantity: 1,
+			},
+			{
+				flavorName: 'Апельсиновый брауни',
+				quantity: 1,
+			},
+			{
+				flavorName: 'Лавандовая черника',
+				quantity: 1,
+			},
+			{
+				flavorName: 'Фундучное пралине',
+				quantity: 1,
+			},
+		],
+		typeHoliday: 'victoryDay',
 		prodDetails: prodDetailsData['eclairs'],
 	},
-	a: {
-		id: ,
-		title: '',
+	forMost: {
+		id: 8,
+		title: 'Подарок «Для Самой-Самой»',
 		description: '',
-		price: ,
+		price: 910,
 		gallery: ['/images/eclairs/',],
 		flavor: [
 			{
-				flavorName: '',
-				quantity: ,
-			},],
-		typeHoliday: 'nationalUnityDay',
-		prodDetails: prodDetailsData['eclairs'],
-	},
-	a: {
-		id: ,
-		title: '',
-		description: '',
-		price: ,
-		gallery: ['/images/eclairs/',],
-		flavor: [
+				flavorName: 'Шоколад',
+				quantity: 3,
+			},
 			{
-				flavorName: '',
-				quantity: ,
-			},],
-		typeHoliday: 'nationalUnityDay',
-		prodDetails: prodDetailsData['eclairs'],
-	},
-	a: {
-		id: ,
-		title: '',
-		description: '',
-		price: ,
-		gallery: ['/images/eclairs/',],
-		flavor: [
+				flavorName: 'Эклер Шоколад',
+				quantity: 1,
+			},
 			{
-				flavorName: '',
-				quantity: ,
-			},],
-		typeHoliday: 'nationalUnityDay',
-		prodDetails: prodDetailsData['eclairs'],
-	},
-	a: {
-		id: ,
-		title: '',
-		description: '',
-		price: ,
-		gallery: ['/images/eclairs/',],
-		flavor: [
+				flavorName: 'Эклер Ваниль',
+				quantity: 1,
+			},
 			{
-				flavorName: '',
-				quantity: ,
-			},],
-		typeHoliday: 'nationalUnityDay',
-		prodDetails: prodDetailsData['eclairs'],
-	},
-	a: {
-		id: ,
-		title: '',
-		description: '',
-		price: ,
-		gallery: ['/images/eclairs/',],
-		flavor: [
+				flavorName: 'Эклер Смородина',
+				quantity: 1,
+			},
 			{
-				flavorName: '',
-				quantity: ,
-			},],
-		typeHoliday: 'nationalUnityDay',
+				flavorName: 'Эклер Карамель',
+				quantity: 1,
+			},
+		],
+		typeHoliday: 'childrensDay',
 		prodDetails: prodDetailsData['eclairs'],
 	},
-
 } as const;
