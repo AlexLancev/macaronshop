d22d0633506f195deeeb9c75fd103373292b7c9d
alex <aleksei.lancevv@gmail.com>
2026-01-08 14:58:41 +0500

    create pluralize

---
+++ src/shared/lib/utils/pluralize.ts
@@ -0,0 +1,12 @@
+export const pluralize = (count: number, words: [string, string, string]): string => {
+	if (count % 10 === 1 && count % 100 !== 11) {
+		return words[0];
+	}
+
+	if (count % 10 >= 2 && count % 10 <= 4 && (count % 100 < 10 || count % 100 >= 20)) {
+		return words[1];
+	}
+
+	return words[2];
+};
+
