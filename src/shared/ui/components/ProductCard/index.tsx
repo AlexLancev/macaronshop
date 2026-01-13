1b68436663febd19f0480d44447ab40d5d98895d
alex <aleksei.lancevv@gmail.com>
2026-01-13 20:03:25 +0500

    create PriceCard

--- src/shared/ui/components/ProductCard/index.tsx
+++ src/shared/ui/components/ProductCard/index.tsx
@@ -1,23 +1,46 @@
-import { CATEGORY_PATHS, CategoryPathKey } from "@/shared/lib/constants/catalog/categoryPaths";
+import {
+	CATEGORY_PATHS,
+	CategoryPathKey,
+} from "@/shared/lib/constants/catalog/categoryPaths";
 import { ProductCardType } from "@/shared/lib/constants/catalog/shared/types";
 import Image from "next/image";
 import Link from "next/link";
+import PriceCard from "../PriceCard";
 
 interface ProductCardProps {
 	data: ProductCardType;
 }
 
 export default function ProductCard({ data }: ProductCardProps) {
-	const { title, description, price, typeProduct, slug, gallery, flavor, typeHoliday, productDetails } = data;
+	const {
+		title,
+		description,
+		price,
+		typeProduct,
+		slug,
+		gallery,
+		flavor,
+		typeHoliday,
+		productDetails,
+	} = data;
 
 	return (
-		<li className="border border-black">
-			<Link href={`${CATEGORY_PATHS[typeProduct as CategoryPathKey]}/${slug}`}>
-				<Image src={gallery[0]} alt={title} width={100} height={100} />
-				<h3>{title}</h3>
-				<p>{description}</p>
-				<span>{typeHoliday}</span>
+		<li className="">
+			<Link className="py-4 bg-white block" href={`${CATEGORY_PATHS[typeProduct as CategoryPathKey]}/${slug}`}>
+				<Image
+					className="w-full h-auto max-h-[400px] object-cover"
+					src={gallery[0]}
+					width={300}
+					height={200}
+					loading="lazy"
+					alt={title}
+				/>
+				<div className="p-4">
+					<h3 className="text-xl font-medium mb-2">{title}</h3>
+					<p className="text-gray-500">{description}</p>
+				</div>
 			</Link>
+			<PriceCard price={price} />
 		</li>
-	)
+	);
 }
