e5681e5df594f7b1f8dfc47f4b634f9e55bd8eb7
alex <aleksei.lancevv@gmail.com>
2026-02-18 21:41:12 +0500

    update OrderComposer

--- src/app/(shop)/order-composer/components/OrderComposer/index.tsx
+++ src/app/(shop)/order-composer/components/OrderComposer/index.tsx
@@ -1,11 +1,31 @@
 "use client";
 
-import { useParams } from "next/navigation";
+import { Button } from "@/shared/ui/components/Button";
 import useOrderComposerStore from "../../store";
+import { useParams } from "next/navigation";
 
 export default function OrderComposer() {
 	const { flavors, totalQuantity } = useOrderComposerStore();
+	const { id: maxQuantity } = useParams();
 
-	console.log(flavors, totalQuantity);
-	return <div></div>;
+	return (
+		<div>
+			<h2>
+				Количество вкусов: {totalQuantity} из {maxQuantity}
+			</h2>
+			<ul>
+				{flavors.map((flavor) => (
+					<li key={flavor.id}>
+						{flavor.flavor}
+					</li>
+				))}
+			</ul>
+			<span>
+				Для продолжения количество макарон должно равняться {maxQuantity} штук.
+			</span>
+			<Button>
+				Далее
+			</Button>
+		</div>
+	);
 }
