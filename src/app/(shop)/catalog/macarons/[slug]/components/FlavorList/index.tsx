952641e99b39126c43b02b4d9c1f726ce42fcdc3
alex <aleksei.lancevv@gmail.com>
2026-07-22 21:00:38 +0300

    create FlavorList

--- src/app/(shop)/catalog/macarons/[slug]/components/FlavorList/index.tsx
+++ src/app/(shop)/catalog/macarons/[slug]/components/FlavorList/index.tsx
@@ -1,3 +1,27 @@
-export default function FlavorList() {
-	return <div>FlavorList</div>;
+import type { FlavorType } from "@/shared/lib/api/products";
+import type { ProductFlavor } from "@/shared/lib/constants/catalog/shared/types";
+
+interface FlavorListProps {
+	flavors: readonly ProductFlavor[] | undefined;
+}
+
+export default function FlavorList({ flavors }: FlavorListProps) {
+	if (!flavors || flavors.length === 0) return null;
+
+	return (
+		<>
+			<strong className="mb-2 font-medium text-lg">Вкусы:</strong>
+			<ul>
+				{flavors.map(({ flavorName, quantity }, idx) => (
+					<li
+						key={idx}
+						className="flex justify-between gap-2"
+					>
+						<span>{flavorName}</span>
+						<span>{quantity}</span>
+					</li>
+				))}
+			</ul>
+		</>
+	);
 }
