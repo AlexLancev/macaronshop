ab42af86335a3a5d957d3028fcb7795427650cee
alex <aleksei.lancevv@gmail.com>
2026-08-01 12:20:35 +0300

    fix ProductHeader

--- src/app/(shop)/catalog/macarons/[slug]/components/ProductHeader/index.tsx
+++ src/app/(shop)/catalog/macarons/[slug]/components/ProductHeader/index.tsx
@@ -4,20 +4,21 @@ interface ProductHeaderProps {
 	title: string;
 	description: string;
 }
-
 export default function ProductHeader({
 	title,
 	description,
 }: ProductHeaderProps) {
 	return (
-		<>
+		<div className="mb-5">
 			<Title
 				headingType="xl"
-				className="mb-2 text-left font-semibold text-3xl"
+				className="text-left font-semibold text-[32px] leading-[40px] tracking-[-0.5px] mb-3 text-[rgb(49,49,49)]"
 			>
 				{title}
 			</Title>
-			<p>{description}</p>
-		</>
+			<p className="text-[16px] leading-[22px] text-[rgb(89,89,89)]">{description}</p>
+		</div>
 	);
 }
+
+
