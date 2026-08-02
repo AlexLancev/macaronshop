e4474eb0a6ff21be303106271325e7e53c70b4fa
alex <aleksei.lancevv@gmail.com>
2026-08-02 12:11:14 +0300

    update FlavorList

--- src/app/(shop)/catalog/macarons/[slug]/components/FlavorList/index.tsx
+++ src/app/(shop)/catalog/macarons/[slug]/components/FlavorList/index.tsx
@@ -9,19 +9,21 @@ export default function FlavorList({ flavors }: FlavorListProps) {
 	if (!flavors || flavors.length === 0) return null;
 
 	return (
-		<>
-			<strong className="mb-2 font-medium text-lg">Вкусы:</strong>
+		<div className="bg-[#fcfcfc] rounded-md p-4 mb-5">
+			<strong className="block text-lg font-semibold mb-2">Вкусы:</strong>
 			<ul>
 				{flavors.map(({ flavorName, quantity }, idx) => (
 					<li
 						key={idx}
-						className="flex justify-between gap-2"
+						className={`flex items-center justify-between py-[2px] text-sm space-y-2 ${
+							idx !== flavors.length - 1 ? "border-b border-[#f0f0f0]" : ""
+						}`}
 					>
 						<span>{flavorName}</span>
 						<span>{quantity}</span>
 					</li>
 				))}
 			</ul>
-		</>
+		</div>
 	);
 }
