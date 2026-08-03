1873f35ef1e885fef4c53eeec8365ec9280cf611
alex <aleksei.lancevv@gmail.com>
2026-08-03 21:15:21 +0300

    update ProductActions

--- src/app/(shop)/catalog/macarons/[slug]/components/ProductActions/index.tsx
+++ src/app/(shop)/catalog/macarons/[slug]/components/ProductActions/index.tsx
@@ -1,3 +1,22 @@
-export default function ProductActions() {
-	return <div>ProductActions</div>;
+import { ShoppingCartIcon } from "lucide-react";
+
+import { Button } from "@/shared/ui/components/Button";
+
+interface ProductActionsProps {
+	price: number;
+}
+
+
+const RUB_SYMBOL = "₽";
+
+export default function ProductActions({ price }: ProductActionsProps) {
+	return (
+		<div className="bg-[rgb(252,252,252)] rounded-md p-6 flex items-center justify-between mb-5">
+			<span className="font-semibold text-[24px] leading-[29px] text-[#E44876]">{price} {RUB_SYMBOL}</span>
+			<Button className="flex items-center gap-2 rounded px-3 py-5 bg-[#E44876] hover:bg-[#d23769] transition-colors justify-center">
+				<ShoppingCartIcon className="w-5 h-5" />
+				Добавить в корзину
+			</Button>
+		</div>
+	)
 }
