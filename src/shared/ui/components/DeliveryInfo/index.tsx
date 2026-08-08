0ca41865e5b85ab5130662604f1648412dce4f18
alex <aleksei.lancevv@gmail.com>
2026-08-08 22:12:55 +0300

    update DeliveryInfo

--- src/shared/ui/components/DeliveryInfo/index.tsx
+++ src/shared/ui/components/DeliveryInfo/index.tsx
@@ -2,12 +2,13 @@ import { deliveryConstants } from "./constants";
 import { iconRender } from "@/shared/lib/constants/icons";
 
 export default function DeliveryInfo() {
+	
 	return (
-		<ul className="flex flex-col gap-4">
+		<ul className="bg-[#fcfcfc] rounded-lg p-6 flex flex-col gap-7">
 			{deliveryConstants.map(({ icon, text }, idx) => (
-				<li key={idx}>
+				<li key={idx} className="flex items-start gap-5">
 					{iconRender[icon]()}
-					<span>{text}</span>
+					<span className="text-[#313131] text-base leading-[22px] whitespace-pre-line">{text}</span>
 				</li>
 			))}
 		</ul>
